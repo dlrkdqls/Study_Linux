5dbccdb600a233ebc1506e4fc83618534f33a738
dlrkdqls <kangbin.lee94@gmail.com>
2025-07-14 18:04:21 +0900

    과제완료

---
+++ codes/cd_pwd_ls.sh
@@ -0,0 +1,33 @@
+ic  Pictures  Public  Templates  Videos
+[kangbeenlee@localhost ~]$ cd..
+bash: cd..: command not found...
+[kangbeenlee@localhost ~]$ cd ..
+[kangbeenlee@localhost home]$ ^C
+bash: :s^C: substitution failed
+[kangbeenlee@localhost home]$ ^C
+bash: :s^C: substitution failed
+[kangbeenlee@localhost home]$ pwd
+/home
+[kangbeenlee@localhost home]$ cd /home/kangbeenlee
+[kangbeenlee@localhost ~]$ ^C
+bash: :s^C: substitution failed
+[kangbeenlee@localhost ~]$ cd ..
+[kangbeenlee@localhost home]$ pwd
+/home
+[kangbeenlee@localhost home]$ cd
+[kangbeenlee@localhost ~]$ ^C
+bash: :s^C: substitution failed
+[kangbeenlee@localhost ~]$ cd /home/kagnbeenlee/Pictures
+bash: cd: /home/kagnbeenlee/Pictures: No such file or directory
+[kangbeenlee@localhost ~]$ ls
+Desktop  Documents  Downloads  Music  Pictures  Public  Templates  Videos
+[kangbeenlee@localhost ~]$ cd Pictures
+[kangbeenlee@localhost Pictures]$ pwd
+/home/kangbeenlee/Pictures
+[kangbeenlee@localhost Pictures]$ ls
+[kangbeenlee@localhost Pictures]$ cd ..
+[kangbeenlee@localhost ~]$ cd Music
+[kangbeenlee@localhost Music]$ pwd
+/home/kangbeenlee/Music
+[kangbeenlee@localhost Music]$ ls
+[kangbeenlee@localhost Music]$ 
