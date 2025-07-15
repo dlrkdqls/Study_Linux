607a51e42b6a73493928587511e38006f9c8392b
dlrkdqls <kangbin.lee94@gmail.com>
2025-07-15 10:06:28 +0900

    퀘스트 완료

--- codes/cd_pwd_ls.sh
+++ codes/cd_pwd_ls.sh
@@ -1,33 +1,22 @@
-ic  Pictures  Public  Templates  Videos
-[kangbeenlee@localhost ~]$ cd..
-bash: cd..: command not found...
-[kangbeenlee@localhost ~]$ cd ..
-[kangbeenlee@localhost home]$ ^C
-bash: :s^C: substitution failed
-[kangbeenlee@localhost home]$ ^C
-bash: :s^C: substitution failed
-[kangbeenlee@localhost home]$ pwd
-/home
-[kangbeenlee@localhost home]$ cd /home/kangbeenlee
-[kangbeenlee@localhost ~]$ ^C
-bash: :s^C: substitution failed
-[kangbeenlee@localhost ~]$ cd ..
-[kangbeenlee@localhost home]$ pwd
-/home
-[kangbeenlee@localhost home]$ cd
-[kangbeenlee@localhost ~]$ ^C
-bash: :s^C: substitution failed
-[kangbeenlee@localhost ~]$ cd /home/kagnbeenlee/Pictures
-bash: cd: /home/kagnbeenlee/Pictures: No such file or directory
+[kangbeenlee@localhost ~]$ pwd
+/home/kangbeenlee
+#경로확인
 [kangbeenlee@localhost ~]$ ls
 Desktop  Documents  Downloads  Music  Pictures  Public  Templates  Videos
-[kangbeenlee@localhost ~]$ cd Pictures
-[kangbeenlee@localhost Pictures]$ pwd
-/home/kangbeenlee/Pictures
-[kangbeenlee@localhost Pictures]$ ls
-[kangbeenlee@localhost Pictures]$ cd ..
-[kangbeenlee@localhost ~]$ cd Music
+$홈 내용 확인
+[kangbeenlee@localhost ~]$ cd /home/kangbeenlee/Music
+#하위 디렉토리 Music으로 접근
 [kangbeenlee@localhost Music]$ pwd
 /home/kangbeenlee/Music
-[kangbeenlee@localhost Music]$ ls
-[kangbeenlee@localhost Music]$ 
+#Music 접근했는지 경로 확인
+[kangbeenlee@localhost Music]$ cd ..
+#상위 디렉토리로 이동
+[kangbeenlee@localhost ~]$ cd /home/hs/Pictures
+bash: cd: /home/hs/Pictures: No such file or directory
+#하위 디렉토리 Pictures로 접근
+[kangbeenlee@localhost ~]$ pwd
+/home/kangbeenlee
+#Pictures에 접근했는지 경로 확인
+[kangbeenlee@localhost ~]$ ls
+Desktop  Documents  Downloads  Music  Pictures  Public  Templates  Videos
+#Pictures 내용 확인
