a52628518ae11422e417b62667b28320b58be1c0
dlrkdqls <kangbin.lee94@gmail.com>
2025-07-15 12:23:06 +0900

    두번째 올리기

---
+++ codes/10_powershell.sh
@@ -0,0 +1,19 @@
+PS C:\Users\Administrator>  cd C:\
+PS C:\> cd C:\Develops
+PS C:\Develops> pwd
+
+Path
+----
+C:\Develops
+
+
+PS C:\Develops> ls
+
+
+    디렉터리: C:\Develops
+
+
+Mode                 LastWriteTime         Length Name
+----                 -------------         ------ ----
+d-----      2025-07-14   오후 4:23                Downloads
+d-----      2025-07-15   오전 9:58                VMs
